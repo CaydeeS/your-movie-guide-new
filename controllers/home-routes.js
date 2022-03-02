c0e1cbffe69ed2013be55881856a39e789d063b2
Yed Star <chstokrp@gmail.com>
2022-03-02 03:21:22 -0800

    handlebars correction and also separated nav into different pages'

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,64 +1,8 @@
 const router = require('express').Router();
-const { index } = require('../models');
+// const { index } = require('../models');
 // Import the custom middleware
-const withAuth = require('../utils/auth');
+// const withAuth = require('../utils/auth');
 
-// // GET all galleries for homepage
-// router.get('/', async (req, res) => {
-//   try {
-//     const dbGalleryData = await Gallery.findAll({
-//       include: [
-//         {
-//           model: Painting,
-//           attributes: ['filename', 'description'],
-//         },
-//       ],
-//     });
-
-//     const galleries = dbGalleryData.map((gallery) =>
-//       gallery.get({ plain: true })
-//     );
-
-//     res.render('main.handlebars', {
-//       galleries,
-//       loggedIn: req.session.loggedIn,
-//     });
-//   } catch (err) {
-//     console.log(err);
-//     res.status(500).json(err);
-//   }
-// });
-
-// // GET one gallery
-// // Use the custom middleware before allowing the user to access the gallery
-// router.get('/gallery/:id', withAuth, async (req, res) => {
-//   try {
-//     const dbGalleryData = await Gallery.findByPk(req.params.id, {
-//       include: [
-//         {
-//           model: Painting,
-//           attributes: [
-//             'id',
-//             'title',
-//             'artist',
-//             'exhibition_date',
-//             'filename',
-//             'description',
-//           ],
-//         },
-//       ],
-//     });
-
-//     const gallery = dbGalleryData.get({ plain: true });
-//     res.render('gallery', { gallery, loggedIn: req.session.loggedIn });
-//   } catch (err) {
-//     console.log(err);
-//     res.status(500).json(err);
-//   }
-// });
-
-// GET one painting
-// Use the custom middleware before allowing the user to access the painting
 
 
 router.get('/', (req, res) => {
@@ -70,13 +14,24 @@ router.get('/', (req, res) => {
   res.render('homepage');
 });
 
+router.get('/watchlist', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
+
+  res.render('watchlist');
+});
+
 router.get('/login', (req, res) => {
   if (req.session.loggedIn) {
-    res.redirect('/login');
+    res.redirect('/');
     return;
   }
 
   res.render('login');
 });
+
+
 
 module.exports = router;
