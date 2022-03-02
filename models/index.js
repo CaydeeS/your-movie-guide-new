4abdc0c143578790faf954c8afc4b4659a6232d7
Yed Star <chstokrp@gmail.com>
2022-03-01 20:39:15 -0800

    handlebars cleanup

--- models/index.js
+++ models/index.js
@@ -1,16 +1,8 @@
 const User = require('./User');
-const Gallery = require('./Gallery');
-const Painting = require('./Painting');
 const Movies = require('./Movies');
 const Genre = require('./Genre');
 
-Gallery.hasMany(Painting, {
-  foreignKey: 'movies_id',
-});
 
-Painting.belongsTo(Gallery, {
-  foreignKey: 'movies_id',
-});
 
 Movies.hasMany(Genre, {
   foreignKey: 'movies_id',
