95a83c1031355b943fac8faccde8d5d93cc12ee1
Yed Star <chstokrp@gmail.com>
2022-03-05 00:32:10 -0800

    home route working, login working

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -3,7 +3,14 @@ const { Movies, Genre } = require('../models');
 // Import the custom middleware
 const withAuth = require('../utils/auth');
 
+router.get('/', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
 
+  res.render('homepage');
+});
 
 router.get('/', async (req, res) => {
   try {
